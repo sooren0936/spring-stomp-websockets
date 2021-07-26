5b78f43a372881357f6e16fc35696168cdc0a4e5
Suren Kalaychyan <kalaychyan.s@firstmk.ru>
2021-07-26 13:35:36 +0300

    Add entity Greeting

---
+++ src/main/java/com/example/springstompwebsockets/entity/Greeting.java
@@ -0,0 +1,24 @@
+package com.example.springstompwebsockets.entity;
+
+/**
+ * @author Suren Kalaychyan
+ */
+public class Greeting {
+
+    private String content;
+
+    public Greeting() {
+    }
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
