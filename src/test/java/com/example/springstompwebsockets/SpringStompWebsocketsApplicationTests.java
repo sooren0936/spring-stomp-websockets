fad5602559635740707462356a5942102ed7018b
Suren Kalaychyan <kalaychyan.s@firstmk.ru>
2021-07-26 13:38:44 +0300

    Add sample file for tests

---
+++ src/test/java/com/example/springstompwebsockets/SpringStompWebsocketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springstompwebsockets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStompWebsocketsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
