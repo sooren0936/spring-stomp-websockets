032326bde763b82d79c2e63b400b30fe16047d18
Suren Kalaychyan <kalaychyan.s@firstmk.ru>
2021-07-26 13:35:55 +0300

    Add controller GreetingController

---
+++ src/main/java/com/example/springstompwebsockets/controller/GreetingController.java
@@ -0,0 +1,22 @@
+package com.example.springstompwebsockets.controller;
+
+import com.example.springstompwebsockets.entity.Greeting;
+import com.example.springstompwebsockets.entity.HelloMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+/**
+ * @author Suren Kalaychyan
+ */
+@Controller
+public class GreetingController {
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000);
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+}
