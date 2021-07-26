055a856a4be39561e9573ade5e44d5c2f58a01c8
Suren Kalaychyan <kalaychyan.s@firstmk.ru>
2021-07-26 13:12:06 +0300

    Initialization application

---
+++ src/main/java/com/example/springstompwebsockets/SpringStompWebsocketsApplication.java
@@ -0,0 +1,12 @@
+package com.example.springstompwebsockets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStompWebsocketsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStompWebsocketsApplication.class, args);
+    }
+}
