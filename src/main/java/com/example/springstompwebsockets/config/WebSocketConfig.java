35165a41330c69e243d0e74ddeafa2a94ab8a046
Suren Kalaychyan <kalaychyan.s@firstmk.ru>
2021-07-28 19:03:42 +0300

    Update registerStompEndPoints with setAllowedOrigins

--- src/main/java/com/example/springstompwebsockets/config/WebSocketConfig.java
+++ src/main/java/com/example/springstompwebsockets/config/WebSocketConfig.java
@@ -14,13 +14,13 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
+    public void configureMessageBroker(final MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
         config.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/websocket").withSockJS();
+    public void registerStompEndpoints(final StompEndpointRegistry registry) {
+        registry.addEndpoint("/websocket").setAllowedOrigins("*").withSockJS();
     }
 }
